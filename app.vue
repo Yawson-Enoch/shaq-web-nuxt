5e6c7525a4968a3b3aa9e03fc6664b7e1bad06c4
Yawson-Enoch <enochgybex25@gmail.com>
2024-09-12 21:38:26 +0000

    chore: project setup

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container py-4 md:py-12">
+    <div class="flex gap-x-4">
+      <Button variant="default">Partnership</Button>
+      <Button variant="destructive">Riders</Button>
+      <Button variant="ghost">About</Button>
+      <Button variant="link">This is a button</Button>
+      <Button variant="outline">This is a button</Button>
+      <Button variant="secondary">This is a button</Button>
+    </div>
+  </div>
+</template>
